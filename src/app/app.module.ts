0ffa91369ac16e371bfd2abdf2d83adec53966e5
PrzemyslawSarnacki <przemos100@gmail.com>
2020-05-21 23:00:53 +0200

    Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+// import { DirectivesModule } from './directives/directives.module';
+
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,8 +13,17 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -17,11 +32,26 @@ import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
     HeaderComponent,
     FooterComponent,
     MainComponent,
-    NavComponent
+    NavComponent,
+    GalleryComponent,
+    NavbarComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    // DirectivesModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
     BrowserModule,
     AnimateOnScrollModule.forRoot(),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
