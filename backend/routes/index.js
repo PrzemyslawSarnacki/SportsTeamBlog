272e76dda77bb1a3b2a56043392a1bb8e02705a6
PrzemyslawSarnacki <przemos100@gmail.com>
2020-05-27 16:58:42 +0200

    Refactored to Arrow functions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,28 +1,28 @@
 var Category = require("../models/category");
 var Post = require("../models/post");
 
-router.get('/category', function(req, res, next) {
+router.get('/category', (req, res, next) => {
     Category.find(function (err, categories) {
       if (err) return next(err);
       res.json(categories);
     });
   });
   
-  router.get('/bycategory/:id', function(req, res, next) {
+  router.get('/bycategory/:id', (req, res, next) => {
     Post.find({category: req.params.id}, function (err, posts) {
       if (err) return next(err);
       res.json(posts);
     });
   });
   
-  router.get('/post', function(req, res, next) {
+  router.get('/post', (req, res, next) => {
     Post.find(function (err, posts) {
       if (err) return next(err);
       res.json(posts);
     });
   });
   
-  router.get('/post/:id', function(req, res, next) {
+  router.get('/post/:id', (req, res, next) => {
     Post.findById(req.params.id, function (err, post) {
       if (err) return next(err);
       res.json(post);
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,35 +2,35 @@ var express = require('express');
 var router = express.Router();
 var Post = require("../models/category");
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
         Post.find(function (err, posts) {
         if (err) return next(err);
         res.json(posts);
       });
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', (req, res, next) => {
       Post.findById(req.params.id, function (err, post) {
         if (err) return next(err);
         res.json(post);
       });
   });
 
-  router.post('/', function(req, res, next) {
+  router.post('/', (req, res, next) => {
       Post.create(req.body, function (err, post) {
         if (err) return next(err);
         res.json(post);
       });
   });
 
-  router.put('/:id', function(req, res, next) {
+  router.put('/:id', (req, res, next) => {
       Post.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
         if (err) return next(err);
         res.json(post);
       });
   });
 
-  router.delete('/:id', function(req, res, next) {
+  router.delete('/:id', (req, res, next) => {
       Post.findByIdAndRemove(req.params.id, req.body, function (err, post) {
         if (err) return next(err);
         res.json(post);
